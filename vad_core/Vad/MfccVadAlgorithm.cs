50fdd0e6c26aac22218538e529d1ef865c3df3c3
lonzoVS <vadshapran@gmail.com>
2017-08-12 15:32:23 +0300

    commentary for mfcc vad algorithm

--- vad_core/Vad/MfccVadAlgorithm.cs
+++ vad_core/Vad/MfccVadAlgorithm.cs
@@ -30,6 +30,7 @@ namespace vad_core.Vad
             this.envMaskPath = envMaskPath;
         }
 
+        
         public void PerformVad(double[] samples)
         {
             var mfccList = PrepareMfcc(samples);
@@ -50,28 +51,10 @@ namespace vad_core.Vad
         }
 
 
-
-        private double[] ComputeSimilarity(double[] mfccList, double[] avrgNoise)
-        {
-            List<double> normalized = new List<double>();
-            List<double> similarity = new List<double>();
-            foreach (double[] Ci in mfccList.ToArray().Split(32))
-            {
-                var newAmounts = Ci.Select(a => a * (1.0 - p)).ToList();
-                var Cno = avrgNoise.Zip(newAmounts, (a, y) => a + y);
-                var coeffCorr = 1.0 - MathExtensions.ComputeCoeff(Ci.ToArray(), Cno.ToArray());
-                similarity.Add(coeffCorr);
-            }
-            double length = similarity.Sum(a => a * a);
-            length = Math.Sqrt(length);
-            foreach (double ddd in similarity)
-            {
-                normalized.Add(ddd / length);
-            }
-            return normalized.ToArray();
-
-        }
-
+        //Frame = 256;
+        //windowing:  Multiplied each frames by a Hamming window;
+        //computing FFT;
+        //computing MFFC from FTT.
         private double[] PrepareMfcc(double[] samples)
         {
             DSP dsp = new DSP();
@@ -101,6 +84,7 @@ namespace vad_core.Vad
 
         }
 
+        //Calculating the MFCC vector of all frames, assuming the first 10 frames as background noise.
         private double[,] ComputeBackgroundNoise(double[] mfccList)
         {
             var noiseMFCC = mfccList.Take(10 * mfccSize).ToList();
@@ -113,6 +97,7 @@ namespace vad_core.Vad
             return arrBUF;
         }
 
+        //Gettin avg value of the first ten frames = initial MFCC vector of background noise.
         private double[] ComputeAverageNoise(double[,] backNoise)
         {
             List<double> avrgNoise = new List<double>();
@@ -123,7 +108,29 @@ namespace vad_core.Vad
             avrgNoise = avrgNoise.Select(a => a * p).ToList();
             return avrgNoise.ToArray();
         }
-        
+
+        //Updating background noise vector with c_no = pc_+(1-p)c_i formula;
+        //computing similarity between c_no(background noise vector) and current mfcc vector;
+        private double[] ComputeSimilarity(double[] mfccList, double[] avrgNoise)
+        {
+            List<double> normalized = new List<double>();
+            List<double> similarity = new List<double>();
+            foreach (double[] Ci in mfccList.ToArray().Split(32))
+            {
+                var newAmounts = Ci.Select(a => a * (1.0 - p)).ToList();
+                var Cno = avrgNoise.Zip(newAmounts, (a, y) => a + y);
+                var coeffCorr = 1.0 - MathExtensions.ComputeCoeff(Ci.ToArray(), Cno.ToArray());
+                similarity.Add(coeffCorr);
+            }
+            double length = similarity.Sum(a => a * a);
+            length = Math.Sqrt(length);
+            foreach (double ddd in similarity)
+            {
+                normalized.Add(ddd / length);
+            }
+            return normalized.ToArray();
+
+        }
 
     }
 }
